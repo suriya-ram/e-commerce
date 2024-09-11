6643a81fca765d2a46fd74cf02463a5dd44cd7e3
suriya-ram <suriyaram123.zra@gmail.com>
2024-09-11 21:26:38 +0530

    first commit

---
+++ shopping-cart-spring-boot-main/src/main/java/com/ecom/model/Category.java
@@ -0,0 +1,74 @@
+package com.ecom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+@Entity
+public class Category {
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(Integer id, String name, String imageName, Boolean isActive) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.imageName = imageName;
+		this.isActive = isActive;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	private String imageName;
+
+	private Boolean isActive;
+
+}
